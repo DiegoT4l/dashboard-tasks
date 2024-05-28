ad6c0dd15529e25525d93889b6402898406a700f
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2024-05-27 23:47:07 -0600

    ""

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// Import our custom CSS
+import '../css/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert'
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from 'bootstrap'
+
+// Import our custom JS
+import './main.mjs'
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,54 @@
-const path = require('path');
+'use strict'
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/js/main.js',
+    mode: 'development',
+    entry: './src/js/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
     }
-};
+}
